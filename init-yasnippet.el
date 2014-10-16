fed31b791ee8c0ad223ae72f45aaa9c3e20e9518
guofei <guofei1986@gmail.com>
2014-10-16 16:16:21 +0900

    remove yasnippet-snippets

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,11 +1,11 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 
 (require 'yasnippet)
-(setq yas-snippet-dirs
-      '("~/.emacs.d/plugins/yasnippet-snippets"
-	"~/.emacs.d/plugins/yasnippets-rails/rails-snippets"
-        "~/.emacs.d/plugins/yasnippet/snippets"
-	))
+;;(setq yas-snippet-dirs
+;;      '("~/.emacs.d/plugins/yasnippet-snippets"
+;;	"~/.emacs.d/plugins/yasnippets-rails/rails-snippets"
+;;        "~/.emacs.d/plugins/yasnippet/snippets"
+;;	))
 (yas-global-mode 1)
 
 (provide 'init-yasnippet)
