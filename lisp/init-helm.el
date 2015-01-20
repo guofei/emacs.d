6861861c71598ff2be7472799933ed1a63ce0718
guofei <guofei1986@gmail.com>
2015-01-20 15:57:56 +0900

    change helm config

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -2,21 +2,31 @@
 (require 'helm)
 (require 'helm-config)
 
-(when (require 'popwin)
-  (setq display-buffer-function 'popwin:display-buffer)
-  (push '("^\*helm .+\*$" :regexp t) popwin:special-display-config))
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
 
-(setq helm-split-window-default-side 'right)
+(global-set-key (kbd "C-x a") 'helm-mini)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
 
-(defun helm-my-buffers ()
-  (interactive)
-  (helm-other-buffer '(helm-c-source-buffers-list
-                       helm-c-source-recentf
-                       helm-c-source-files-in-current-dir
-                       helm-c-source-buffer-not-found)
-                     "*helm-my-buffers*"))
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
-(define-key global-map (kbd "C-x a") 'helm-my-buffers)
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           nil ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
+
 ;;search all files : helm-for-files
 
 (provide 'init-helm)
