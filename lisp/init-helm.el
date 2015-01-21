746a8e8bd8a0f0cc3db30d9d6e2ac0d1378ae951
guofei <guofei1986@gmail.com>
2015-01-21 13:27:37 +0900

    upgrade auto complete

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -18,10 +18,10 @@
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
 
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+(setq helm-split-window-in-side-p           nil ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     nil ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t   ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8   ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
 
 ;; my buffers
