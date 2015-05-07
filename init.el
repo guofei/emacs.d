8e6d69f7408bba62ebb95b6d801cf9748f957bdf
guofei <guofei1986@gmail.com>
2015-05-07 16:51:56 +0900

    change package-archives

--- init.el
+++ init.el
@@ -52,6 +52,7 @@
 ;;)
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
