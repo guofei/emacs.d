f2e546f0059119e05bd0c69b92e7df0cb9f0a5cc
guofei <guofei1986@gmail.com>
2015-07-05 23:09:46 +0900

    web-mode yas

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -9,4 +9,10 @@
 	))
 (yas-global-mode 1)
 
+(add-hook 'web-mode-hook
+          #'(lambda ()
+              (yas-activate-extra-mode 'html-mode)
+              (yas-activate-extra-mode 'rails-mode)
+              ))
+
 (provide 'init-yasnippet)
