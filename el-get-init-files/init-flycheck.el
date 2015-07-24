861631fc5a164bca006dbbb7dd6f804bcfae1b00
guofei <guofei1986@gmail.com>
2015-07-25 00:54:51 +0900

    add ruby-mode-hook to helm-gtags-mode

--- el-get-init-files/init-flycheck.el
+++ el-get-init-files/init-flycheck.el
@@ -1,6 +1,9 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;;; c++11
 (add-hook 'c++-mode-hook (lambda () (setq flycheck-clang-language-standard "c++11")))
 
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
