27b2ca23cbb601f29aa6dd1f23469b2cad5173c8
guofei <guofei1986@gmail.com>
2015-11-02 17:30:06 +0900

    flycheck

--- el-get-init-files/init-flycheck.el
+++ el-get-init-files/init-flycheck.el
@@ -1,24 +1,31 @@
+(require 'flycheck)
+
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;;; c++11
 (add-hook 'c++-mode-hook (lambda () (setq flycheck-clang-language-standard "c++11")))
 
 ;; disable jshint since we prefer eslint checking
-;; (setq-default flycheck-disabled-checkers
-;;               (append flycheck-disabled-checkers
-;;                       '(javascript-jshint)))
-
-
-;; use eslint with web-mode for jsx files
-;; (flycheck-add-mode 'javascript-eslint 'web-mode)
+;; (eval-after-load 'flycheck
+(setq-default flycheck-disabled-checkers
+                 (append flycheck-disabled-checkers
+                         '(javascript-jshint)))
 
 
 ;; disable json-jsonlist checking for json files
-;; (setq-default flycheck-disabled-checkers
-;;               (append flycheck-disabled-checkers
-;;                       '(json-jsonlist)))
+;; (eval-after-load 'flycheck
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(json-jsonlist)))
+
+
+;; use eslint with web-mode for jsx files
+;; (eval-after-load 'flycheck
+(flycheck-add-mode 'javascript-eslint 'web-mode)
 
 
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
--- el-get-init-files/init-web-mode.el
+++ el-get-init-files/init-web-mode.el
@@ -8,11 +8,17 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 
 
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
+;; adjust indents for web-mode to 2 spaces
+(defun web-mode-indent-hook ()
+  "Hooks for Web mode. Adjust indents"
+    ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'web-mode-indent-hook)
 
 
 ;; (setq web-mode-ac-sources-alist
