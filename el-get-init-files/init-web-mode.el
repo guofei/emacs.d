7dc389a8ad688f3b55ad8ff67b8c13a59c53faa5
guofei <guofei1986@gmail.com>
2015-11-04 00:01:15 +0900

    change web-mode-attr-indent-offset

--- el-get-init-files/init-web-mode.el
+++ el-get-init-files/init-web-mode.el
@@ -16,10 +16,11 @@
 (defun web-mode-indent-hook ()
   "Hooks for Web mode. Adjust indents"
     ;;; http://web-mode.org/
+  (setq web-mode-attr-indent-offset 2)
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
-(add-hook 'web-mode-hook  'web-mode-indent-hook)
+(add-hook 'web-mode-hook 'web-mode-indent-hook)
 
 
 ;; http://cha1tanya.com/2015/06/20/configuring-web-mode-with-jsx.html
@@ -40,7 +41,7 @@
 ;;         ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
 
 (add-hook 'web-mode-before-auto-complete-hooks
-          '(lambda ()
+          (lambda ()
              (let ((web-mode-cur-language
                     (web-mode-language-at-pos)))
                (if (string= web-mode-cur-language "html")
