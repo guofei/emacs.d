f541bd39eaef0393aff121e150dee88c2eb72694
guofei <guofei1986@gmail.com>
2015-12-23 14:24:16 +0900

    add elixir-mode

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
 (el-get-bundle haml-mode)
 (el-get-bundle solarized-emacs)
 ;;(el-get-bundle color-theme-solarized)
+(el-get-bundle elixir)
 
 ;;(el-get-bundle flycheck)
 (el-get-bundle elpa:flycheck)
