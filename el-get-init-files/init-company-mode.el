0d1e9fad73204edde6d7d81f02382169937735bb
guofei <guofei1986@gmail.com>
2016-01-07 17:44:23 +0900

    chore: change company mode's min prefix length

--- el-get-init-files/init-company-mode.el
+++ el-get-init-files/init-company-mode.el
@@ -5,5 +5,5 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 (setq company-idle-delay 0)
-(setq company-minimum-prefix-length 2)
+(setq company-minimum-prefix-length 3)
 (setq company-selection-wrap-around t)
